77761f5357baa880ae2b9bf98207308e758a926f
Vladimir Sizonenko <dilfin07@icloud.com>
2020-09-14 11:46:03 +0400

    Исправление ошибок

--- Отценки.py
+++ Отценки.py
@@ -8,11 +8,15 @@ school = [{'school_class': '4a', 'scores': [3, 4, 4, 5, 2]}, {'school_class': '9
           {'school_class': '2b', 'scores': [5, 4, 2, 5, 3]}]
 
 print("Средний бал по классу:")
-for i in school:
-    print(i["school_class"], "-", sum(i for i in i["scores"]) / len(i["scores"]))  # средний бал по классу
-print("Средний бал по школе:")
+for row in school:
+    print(row["school_class"], sum(row["scores"])/len(row["scores"]))
+print("Средний бал по всей школе:")
 a = []
-for i in school:
-    s = (i["scores"])
-    a.append(s)
-print((sum(map(sum, a))) / (sum(map(len, a))))
+for line in school:
+    for element in line["scores"]:
+        a.append(element)
+print(sum(a)/len(a))# средний бал по школе
+
+
+
+
